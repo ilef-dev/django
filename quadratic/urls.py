b1358300ab7f310360027351f185f9312b8a98dd
ilef-dev <efanovis@yandex.ru>
2024-12-13 20:31:00 +0300

    Начал заново

--- quadratic/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.input_form, name='input_form'),
-    path('solve/', views.solve_quadratic, name='solve_quadratic'),
-]
